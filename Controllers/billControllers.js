b879068dc990e43eb6d836b3e767deae15951f2f
chirag <chiragindia2@gmail.com>
2021-11-22 19:48:06 +0530

    Added update bill function

--- Controllers/billControllers.js
+++ Controllers/billControllers.js
@@ -71,4 +71,62 @@ const deleteBill = async (req, res) => {
     }
 };
 
-module.exports = { addBill, deleteBill };
+const updateBill = async (req, res) => {
+    const {
+        id,
+        billId,
+        amount,
+        title,
+        description,
+        date: plainDate,
+    } = req.body;
+
+    if (!billId) {
+        return res.json({ status: "error", error: "Invalid bill ID" });
+    }
+
+    try {
+        const user = await User.findOne({ _id: id, "bills._id": billId });
+
+        if (user) {
+            const bills = user.bills;
+            var prevAmount, prevTitle, prevDescription, prevDate;
+
+            bills.forEach((bill) => {
+                if (bill._id.equals(billId)) {
+                    prevAmount = bill.amount;
+                    prevTitle = bill.title;
+                    prevDescription = bill.description;
+                    prevDate = bill.date;
+                }
+            });
+
+            await User.updateOne(
+                { _id: id, "bills._id": billId },
+                {
+                    $set: {
+                        "bills.$.amount": amount ? amount : prevAmount,
+                        "bills.$.title": title ? title : prevTitle,
+                        "bills.$.description": description
+                            ? description
+                            : prevDescription,
+                        "bills.$.date": plainDate
+                            ? new Date(plainDate)
+                            : prevDate,
+                    },
+                }
+            );
+
+            return res.json({
+                status: "ok",
+                message: "Bill updated successfully",
+            });
+        } else {
+            return res.json({ status: "error", error: "Bill not found" });
+        }
+    } catch (error) {
+        return res.json({ status: "error", error: "Some error occurred" });
+    }
+};
+
+module.exports = { addBill, deleteBill, updateBill };
--- Routes/billRoutes.js
+++ Routes/billRoutes.js
@@ -2,9 +2,14 @@ const express = require("express");
 const router = express.Router();
 const { auth } = require("../Middlewares/authMiddleware");
 
-const { addBill, deleteBill } = require("../Controllers/billControllers");
+const {
+    addBill,
+    deleteBill,
+    updateBill,
+} = require("../Controllers/billControllers");
 
 router.post("/add", auth, addBill);
 router.post("/delete", auth, deleteBill);
+router.post("/update", auth, updateBill);
 
 module.exports = router;
