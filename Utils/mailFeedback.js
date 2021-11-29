a3e33a6888d7ba770ca089e0ae13fa3b1744f691
chirag <chiragindia2@gmail.com>
2021-11-29 20:02:22 +0530

    Changed feedback email to address

--- Utils/mailFeedback.js
+++ Utils/mailFeedback.js
@@ -12,7 +12,7 @@ const sendMail = (userEmail, feedback) => {
 
     const mailOptions = {
         from: process.env.GMAIL_EMAIL,
-        to: process.env.GMAIL_EMAIL,
+        to: process.env.FEEDBACK_EMAIL,
         subject: `Feedback | ${userEmail}`,
         text: feedback,
     };
