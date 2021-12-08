9474945628eddbb3f7f8abd3376b865295ae5999
Chirag <chiraggoyal1910@gmail.com>
2021-12-08 22:40:04 +0530

    Updated OTP email body

--- Utils/mailOtp.js
+++ Utils/mailOtp.js
@@ -10,13 +10,40 @@ const sendMail = (mailTo, otp) => {
         },
     });
 
+    const body = `
+        <div style="text-align: center;">
+            <div style="text-align: center;margin: auto;padding: 50px 30px; width: 80%; max-width:500px; background-color:rgb(0, 138, 153, 0.1); border-radius: 10px;">
+                <h2 style="text-align: center; font-family: 'Trebuchet MS', sans-serif;font-size: xx-large; color: #002E33; font-weight: 600; letter-spacing: 3px;">
+                    Welcome!
+                </h2>
+                <p style="text-align: center; font-family: 'Trebuchet MS', sans-serif;font-size: medium; color: #000;margin-top:-10px;">
+                    We are excited to get you started!
+                </p>
+
+                <h3 style="text-align: center; font-family: 'Trebuchet MS', sans-serif;font-size: x-large; color: #000; font-weight: 500; margin-top: 50px ">
+                    Your One Time Password is
+                </h3>
+                <h2 style="text-align: center; font-family: 'Trebuchet MS', sans-serif;font-size: xxx-large; background-color: #002E33; color: #fff; font-weight: 900; letter-spacing: 7px; margin: 60px auto; width:fit-content; padding: 20px; border-radius:7px;">
+                    ${otp}
+                </h2>
+                <p style="text-align: center; font-family: 'Trebuchet MS', sans-serif;font-size: medium; color: #666;">
+                    OTP is valid for next 30 minutes.
+                </p>
+                <p style="text-align: center; font-family: 'Trebuchet MS', sans-serif;font-size: medium; color: #666;">
+                    Please do not share the OTP or login credentials with anyone.
+                </p>
+                <p style="text-align: center; font-family: 'Trebuchet MS', sans-serif;font-size: large; color: #002E33; margin-top: 30px;">
+                    LinkIt
+                </p>
+            </div>
+        </div>
+`;
+
     const mailOptions = {
         from: process.env.GMAIL_EMAIL,
         to: mailTo,
         subject: "LinkIt OTP",
-        html: `<h1>OTP: ${otp}</h1>
-        <p>OTP is valid for next 30 minutes.</p>
-        <p>Please do not share the OTP or login credentials with anyone.</p>`,
+        html: body,
     };
 
     transporter.sendMail(mailOptions, (error) => {
