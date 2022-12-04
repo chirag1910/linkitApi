664bbfc17b09a037dd9c8192b7c4ff203f4b6d22
Chirag Goyal <chiraggoyal1910@gmail.com>
2022-12-04 15:38:37 +0530

    Added health check route

--- Controllers/miscControllers.js
+++ Controllers/miscControllers.js
@@ -31,6 +31,15 @@ const sendFeedback = async (req, res) => {
     }
 };
 
+const health = (req, res) => {
+    return res.json({
+        status: "ok",
+        code: 200,
+        message: "Server running",
+    });
+};
+
 module.exports = {
     sendFeedback,
+    health,
 };
--- index.js
+++ index.js
@@ -9,17 +9,18 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true, origin: true }));
-app.use(verify);
 
 const userRoutes = require("./Routes/userRoutes");
 const urlGroupRoutes = require("./Routes/urlGroupRoutes");
 const urlRoutes = require("./Routes/urlRoutes");
 const miscRoutes = require("./Routes/miscRouter");
+const { health } = require("./Controllers/miscControllers");
 
-app.use("/user", userRoutes);
-app.use("/urlGroup", urlGroupRoutes);
-app.use("/url", urlRoutes);
-app.use("/misc", miscRoutes);
+app.use("/user", verify, userRoutes);
+app.use("/urlGroup", verify, urlGroupRoutes);
+app.use("/url", verify, urlRoutes);
+app.use("/misc", verify, miscRoutes);
+app.get("/", health);
 
 const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
